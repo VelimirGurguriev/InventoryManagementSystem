2078c036af0d4c0916f6a7a3ff624cdc836d993f
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-06-28 14:56:47 +0300

    added getters and setters for ID and quantity

--- src/SystemManagementInventory/Inventory.java
+++ src/SystemManagementInventory/Inventory.java
@@ -1,13 +1,31 @@
 package SystemManagementInventory;
 
 public class Inventory extends AbstractItem {
-    
+    private String itemId;
+    private int quantity;
+
     public Inventory(String itemName, String category, double price, boolean breakable, boolean perishable) {
         super(itemName, category, price, breakable, perishable);
     }
 
     @Override
     public double calculateValue() {
-        return 0;
+        return getItemPrice() * quantity;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
