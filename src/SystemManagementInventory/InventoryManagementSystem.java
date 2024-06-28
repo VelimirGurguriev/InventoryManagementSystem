e6a3616db88337f3cdfb80306e758533aefc3908
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-06-28 18:09:07 +0300

    implemented method to add items

--- src/SystemManagementInventory/InventoryManagementSystem.java
+++ src/SystemManagementInventory/InventoryManagementSystem.java
@@ -3,12 +3,16 @@ package SystemManagementInventory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InventoryManagementSystem {
+    private List<InventoryItem> inventoryList;
     private BufferedReader reader;
 
     public InventoryManagementSystem() {
         reader = new BufferedReader(new InputStreamReader(System.in));
+        inventoryList = new ArrayList<>();
     }
 
     public void run() {
@@ -18,6 +22,7 @@ public class InventoryManagementSystem {
             switch (choice) {
                 case 1:
                     System.out.println("Add item option");
+                    addItem();
                     break;
                 case 2:
                     System.out.println("Remove item by ID");
@@ -79,5 +84,56 @@ public class InventoryManagementSystem {
         System.out.println("4. Categorize items.");
         System.out.println("5. Place orders.");
         System.out.println("0. Exit.");
+    }
+
+    private void addItem() {
+        try {
+            System.out.print("Enter type (Electronics/Grocery/Fragile): ");
+            String itemType = reader.readLine().trim().toLowerCase();
+            System.out.print("Enter Item ID: ");
+            String itemId = reader.readLine();
+            System.out.print("Enter Item Name: ");
+            String itemName = reader.readLine();
+            System.out.print("Enter Category: ");
+            String category = reader.readLine();
+            System.out.print("Enter Price: ");
+            double price = Double.parseDouble(reader.readLine());
+            System.out.print("Is it Breakable (true/false)? : ");
+            boolean breakable = Boolean.parseBoolean(reader.readLine());
+            System.out.print("Is it Perishable (true/false)? : ");
+            boolean perishable = Boolean.parseBoolean(reader.readLine());
+            System.out.print("Enter Quantity: ");
+            int quantity = Integer.parseInt(reader.readLine());
+
+            InventoryItem item;
+
+            switch (itemType) {
+                case "electronics":
+                    System.out.print("Enter Warranty Years: ");
+                    int warrantyYears = Integer.parseInt(reader.readLine());
+                    item = new ElectronicsItem(itemName, category, price, breakable, perishable, itemId, quantity, warrantyYears);
+                    break;
+                case "grocery":
+                    System.out.print("Enter Expiration Date (dd/mm/yyyy): ");
+                    String expirationDate = reader.readLine();
+                    item = new GroceryItem(itemName, category, price, breakable, perishable, itemId, quantity, expirationDate);
+                    break;
+                case "fragile":
+                    System.out.print("Enter Weight in kilograms: ");
+                    double weight = Double.parseDouble(reader.readLine());
+                    item = new FragileItem(itemName, category, price, breakable, perishable, itemId, quantity, weight);
+                    break;
+                default:
+                    System.out.println("Invalid item type.");
+                    return;
+            }
+
+            inventoryList.add(item);
+            System.out.println("Item added successfully.");
+        } catch (IOException exc) {
+            System.out.println("Error during input: " + exc.getMessage());
+        } catch (NumberFormatException exc) {
+            System.out.println("Invalid input format.");
+        }
     }
 }
