6aac52fd32081134a246ad1cbe3cfe8f091da504
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-06-28 22:25:12 +0300

    rewrote Order class to implement correctly PaymentMethod

--- src/SystemManagementInventory/Order.java
+++ src/SystemManagementInventory/Order.java
@@ -1,33 +1,48 @@
 package SystemManagementInventory;
 
-import java.util.ArrayList;
-import java.util.List;
+import ApplicationConsoleCommerce.PaymentMethod;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Order {
-    private List<InventoryItem> items;
     private double totalOrderAmount;
+    private static int currentOrderCount = 1;
+    private int orderId;
+    private Map<InventoryItem, Integer> items;
+    private PaymentMethod paymentMethod;
 
     public Order() {
-        items = new ArrayList<>();
-        totalOrderAmount = 0.0;
+        this.orderId = currentOrderCount++;
+        this.items = new HashMap<>();
     }
 
     public void addItemToOrder(InventoryItem item, int quantity) {
-        for (int i = 0; i < quantity; i++) {
-            items.add(item);
+        if (items.containsKey(item)) {
+            items.put(item, items.get(item) + quantity);
+        } else {
+            items.put(item, quantity);
         }
         totalOrderAmount += item.getItemPrice() * quantity;
     }
 
-    public void processOrder(Payment payment) {
-        if (payment.getAmount() >= totalOrderAmount) {
-            System.out.println("Order is processed. Total amount is: " + totalOrderAmount + "LV.");
-        } else {
-            System.out.println("Not enough money!. Order failed to process.");
-        }
+    public void processOrder(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+        System.out.println("Order " + orderId + " was paid with " + paymentMethod.getPaymentType());
     }
 
-    public List<InventoryItem> getItems() {
+    public void displayOrder() {
+        System.out.println("Order ID: " + orderId);
+        for (Map.Entry<InventoryItem, Integer> entry : items.entrySet()) {
+            InventoryItem item = entry.getKey();
+            int quantity = entry.getValue();
+            System.out.println(quantity + " of " + item.getItemName()
+                    + " costing " + item.getItemPrice() + "LV. for each");
+        }
+        System.out.println("Total order amount: " + totalOrderAmount + "LV.");
+    }
+
+    public Map<InventoryItem, Integer> getItems() {
         return items;
     }
 
